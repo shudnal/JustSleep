89deb309d6891846e9348193b1694f43c4b4c8f8
Suden <20457922+shudnal@users.noreply.github.com>
2024-03-31 11:01:07 +0300

    assembly version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1f044c3f-2bd7-41a4-981b-5e0e47ba84e3")]
-[assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.3")]
